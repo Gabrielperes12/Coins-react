2e0ba236f2fc75fd5a471939b63ad69775012e39
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-22 15:22:38 -0300

    react

---
+++ coins-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Conversor from './componentes/Conversor'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
